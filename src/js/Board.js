eeb6bffb8bbd9fcd5c927bd79a59a022dd33cfa4
Neill Robson <nlrobson@ncsu.edu>
2016-04-16 11:43:51 -0400

    Add setters and getters for Board

--- src/js/Board.js
+++ src/js/Board.js
@@ -1,8 +1,19 @@
 export default class Board {
-  constructor(row, col) {
+  constructor(row, col, indices) {
     this.row = row;
     this.col = col;
-    this.grid = new Array(row);
-    this.grid.fill(new Array(col));
+    this.grid = new Array(row).fill(new Array(col).fill(0));
+    for (let pair of indices) {
+      console.log(pair);
+      this.setIndex(pair.x, pair.y, 1);
+    }
+  }
+
+  getIndex(x, y) {
+    return this.grid[y][x];
+  }
+
+  setIndex(x, y, val) {
+    this.grid[y][x] = val;
   }
 }
--- src/js/test/board-test.js
+++ src/js/test/board-test.js
@@ -2,8 +2,16 @@ import Board from "../Board";
 import test from "tape";
 
 test('Board constructor', t => {
-  t.plan(2);
-  var b = new Board(10, 20);
+  var b = new Board(10, 20, [
+    {x: 4, y: 4},
+    {x: 8, y: 4},
+    {x: 5, y: 7}
+  ]);
   t.equal(10, b.row);
   t.equal(20, b.col);
+  t.equal(1, b.getIndex(4, 4));
+  t.equal(1, b.getIndex(8, 4));
+  t.equal(1, b.getIndex(5, 7));
+  t.equal(1, b.getIndex(5, 6));
+  t.end();
 });
