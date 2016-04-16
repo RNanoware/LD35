8dee899da979dddc28622151429f0550507ebfdf
Neill Robson <nlrobson@ncsu.edu>
2016-04-16 14:27:57 -0400

    Demonstrate drawing of cursor object

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,14 @@
 import Board from './Board';
+import Cursor from './Cursor';
 
 var canvas = document.getElementById('canvas');
 canvas.height = 480;
 canvas.width = 640;
 var context = canvas.getContext('2d');
 
-var b = new Board(32, 24, [
-  {x: 1, y: 3},
-  {x: 20, y: 3},
-  {x: 1, y: 10},
-  {x: 31, y: 23},
-  {x: 0, y: 0}
-]);
-
 function drawGrid(cellSize) {
+  context.strokeStyle = "gray";
+  context.fillStyle = "black";
   for (let i = 0; i < canvas.width / cellSize; i++) {
     for (let j = 0; j < canvas.height / cellSize; j++) {
       if (b.getIndex(i, j) === 1) {
@@ -25,4 +20,53 @@ function drawGrid(cellSize) {
   }
 }
 
+function drawCursor(cursor, cellSize) {
+  var xCorner = cellSize * cursor.x;
+  var yCorner = cellSize * cursor.y;
+  switch (cursor.side) {
+    case Cursor.side.LEFT:
+      var xStart = xCorner + 1;
+      var yStart = yCorner + 1;
+      var xEnd = xStart;
+      var yEnd = yCorner + cellSize - 1;
+      break;
+    case Cursor.side.RIGHT:
+      var xStart = xCorner + cellSize - 1;
+      var yStart = yCorner + 1;
+      var xEnd = xStart;
+      var yEnd = yCorner + cellSize - 1;
+      break;
+    case Cursor.side.DOWN:
+      var xStart = xCorner + 1;
+      var yStart = yCorner + cellSize - 1;
+      var xEnd = xCorner + cellSize - 1;
+      var yEnd = yStart;
+      break;
+    case Cursor.side.UP:
+      var xStart = xCorner + 1;
+      var yStart = yCorner + 1;
+      var xEnd = xCorner + cellSize - 1;
+      var yEnd = yStart;
+      break;
+  }
+  context.strokeStyle = "red";
+  context.lineWidth = 2;
+  context.beginPath();
+  context.moveTo(xStart, yStart);
+  context.lineTo(xEnd, yEnd);
+  context.closePath();
+  context.stroke();
+}
+
+var b = new Board(32, 24, [
+  {x: 1, y: 3},
+  {x: 20, y: 3},
+  {x: 1, y: 10},
+  {x: 31, y: 23},
+  {x: 0, y: 0}
+]);
+
+var c = new Cursor(b, 1, 1, Cursor.side.UP);
+
 drawGrid(20);
+drawCursor(c, 20);
