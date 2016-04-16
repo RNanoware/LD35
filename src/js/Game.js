e928c25f2eff1f7f8723903f4cab974e01ebb3d1
Neill Robson <nlrobson@ncsu.edu>
2016-04-16 19:16:34 -0400

    Add dummy construction to Game

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,9 +1,29 @@
+import Board from './Board';
+import Cursor from './Cursor';
+
 export default class Game {
   constructor(canvasId) {
     var canvas = document.getElementById(canvasId);
-    var context = canvas.getContext('2d');
-    var gameSize = { x: 640, y: 480 };
-    ({x: canvas.width, y: canvas.height} = gameSize);
+    canvas.width = 640;
+    canvas.height = 480;
+
+    this.context = canvas.getContext('2d');
+
+    this.board = new Board(20, 20);
+    this.board.setAll([
+      {x: 1, y: 3},
+      {x: 19, y: 3},
+      {x: 1, y: 10},
+      {x: 11, y: 18},
+      {x: 0, y: 0}
+    ], 1);
+    this.board.addCursor(new Cursor(this.board, 3, 3, Cursor.side.LEFT));
+  }
+
+  update() {}
+
+  draw() {
+    this.board.draw(this.context, 10, 20, 15, 25);
   }
 
   tick() {
