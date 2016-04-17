c5c505b89b8ff52485718cf56008e18da45392fe
Neill Robson <nlrobson@ncsu.edu>
2016-04-17 13:18:35 -0400

    Adjust viewport for new board shapes

--- src/js/Game.js
+++ src/js/Game.js
@@ -14,15 +14,15 @@ export default class Game {
     // Board 2: Top right
     // Board 3: Bottom right
     // Board 4: Bottom left
-    var board1 = new Board(10, 10);
-    var board2 = new Board(10, 10);
-    var board3 = new Board(10, 10);
-    var board4 = new Board(10, 10);
+    var board1 = new Board(9, 9);
+    var board2 = new Board(9, 1);
+    var board3 = new Board(9, 9);
+    var board4 = new Board(1, 9);
 
-    var cursor1 = new Cursor(board1, 3, 3, Cursor.side.TOP);
-    var cursor2 = new Cursor(board2, 3, 3, Cursor.side.TOP);
-    var cursor3 = new Cursor(board3, 3, 3, Cursor.side.TOP);
-    var cursor4 = new Cursor(board4, 3, 3, Cursor.side.TOP);
+    var cursor1 = new Cursor(board1, 0, 0, Cursor.side.TOP);
+    var cursor2 = new Cursor(board2, 0, 0, Cursor.side.TOP);
+    var cursor3 = new Cursor(board3, 0, 0, Cursor.side.TOP);
+    var cursor4 = new Cursor(board4, 0, 0, Cursor.side.TOP);
     cursor1.next = cursor2;
     cursor2.next = cursor3;
     cursor3.next = cursor4;
@@ -46,28 +46,28 @@ export default class Game {
 
     this.boards.push({
       board: board1,
-      x: 0,
-      y: 0,
-      w: 200,
-      h: 200
+      x: 10,
+      y: 10,
+      w: 180,
+      h: 180
     }, {
       board: board2,
-      x: 210,
-      y: 0,
-      w: 200,
-      h: 200
+      x: 280,
+      y: 10,
+      w: 20,
+      h: 180
     }, {
       board: board3,
-      x: 210,
-      y: 210,
-      w: 200,
-      h: 200
+      x: 200,
+      y: 200,
+      w: 180,
+      h: 180
     }, {
       board: board4,
-      x: 0,
-      y: 210,
-      w: 200,
-      h: 200
+      x: 10,
+      y: 280,
+      w: 180,
+      h: 20
     });
   }
 
