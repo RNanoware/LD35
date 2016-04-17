afc555d91895c44ca36cd1d4f9143177ff7080a0
Neill Robson <nlrobson@ncsu.edu>
2016-04-17 18:43:41 -0400

    Prevent arrow-key scrolling when playing the game

--- src/js/Keyboarder.js
+++ src/js/Keyboarder.js
@@ -3,6 +3,11 @@ export default class Keyboarder {
     this.keyState = {};
     window.addEventListener("keydown", e => this.keyState[e.keyCode] = true);
     window.addEventListener("keyup", e => this.keyState[e.keyCode] = false);
+    document.onkeydown = function(e) {
+      var a = [37, 38, 39, 40];
+      if (a.indexOf(e.which) > -1)
+        e.preventDefault();
+    }
   }
 
   isDown(keyCode) {
